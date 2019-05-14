bd1c511e87285d092c15a2c6681e59fb01f6cab6
LeonardoCella <leonardo.cella@unimi.it>
2019-05-14 15:53:16 +0200

    Fixed bug in protocol

--- adaptiveRank/environment/MAB.py
+++ adaptiveRank/environment/MAB.py
@@ -21,11 +21,13 @@ class MAB(Environment):
         self.fraTop = fraTop
         self.maxDelay = maxDelay
         self.arms = []
-        self.nbArms = self._arm_creation()
 
+        # Arm Creation
+        self.nbArms = self._arm_creation()
         self._armsDelay = zeros(self.nbArms)
         self._armsIndexes = arange(self.nbArms)
-        c_print(2, "MAB.py, INIT: List of {} Bernoulli arms".format(len(self.arms)))
+        assert self._armsIndexes[-1] == self.nbArms -1, "Wrong arm creation"
+        c_print(4, "MAB.py, INIT: Arm Indexes {}".format(self._armsIndexes))
         self._armsStates = zeros(self.nbArms)
 
     def _arm_creation(self):
@@ -38,19 +40,19 @@ class MAB(Environment):
         means = around(array(snp.merge(starting_grid, good_arms)), 2) # evenly round to 2 decimals 
         means = 1 - unique(means)
         c_print(4, "\n=========MAB_INIT=========")
-        c_print(3, "MAB.py, Arm means: {}".format(means))
+        c_print(4, "MAB.py, Arm means: {}".format(means))
         for mu in means:
             tmpArm = Bernoulli(mu, self.gamma, self.maxDelay)
             self.arms.append(tmpArm)
             c_print(1, "MAB INIT: Created arm: {}".format(tmpArm))
         return len(means)
 
-    def compute_states(self, round):
+    def compute_states(self):
         '''Called at every step in the play() method. Manages the trajectory evolution.'''
         assert len(self._armsDelay) == len(self._armsStates), "MAB compute_states: Incoherent size"
         for i, arm, delay in zip(arange(0, self.nbBuckets, 1), self.arms, self._armsDelay):
             self._armsStates[i] = arm.computeState(delay)
-        c_print(1, "\nMAB.py, compute_state() Round {}, Arms states: {}".format(round, self._armsStates))
+        c_print(1, "MAB.py, compute_state() Round {}, Arms states: {}".format(round, self._armsStates))
         return
 
     def play(self, policy, horizon, nbRepetition):
@@ -59,31 +61,33 @@ class MAB(Environment):
 
         result = Result(horizon)
         t = 0
-        while t < horizon:
-            self.compute_states(t)
 
+        while t < horizon:
             # Structured Choice and Feedback 
             choice = policy.choice(self._armsStates)
+
             for c in choice:
-                c_print(4, "Chosen arm: {} at round: {}".format(c, t))
                 reward = self.arms[c].draw(self._armsDelay[c])
+                c_print(2, "Chosen arm: {} at round: {} with rwd {}".format(c, t, reward))
                 policy.update(c, reward, self._armsDelay[c])
                 result.store(t, c, reward)
 
                 # Delays update
                 for i in self._armsIndexes:
                     d = self._armsDelay[i]
+                    # Not the chosen arm and already pulled once
                     if d != 0 and i != c:
-                        self._armsDelay[i] += 1
+                        self._armsDelay[i] = self._armsDelay[i] + 1
+                    # I cannot put it to zero or it seems like an unpulled arm
                     if d > self.maxDelay:
-                        self._armsDelay[i] = 0
+                        self._armsDelay[i] = self.maxDelay + 1
                     if i == c:
                         self._armsDelay[i] = 1
                 t = t + 1
+                self.compute_states()
 
                 # Additional termination condition due to finite horizon
                 if t == horizon:
                     return result
-
 
         return result
